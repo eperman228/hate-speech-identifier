6c2e6f91852f63759c6ecebd7707935ca42cd230
Julie Lawler <143622300+jal355@users.noreply.github.com>
2025-01-21 21:42:37 -0500

    New FeatureGenerator Class

--- log_reg_classifier.py
+++ log_reg_classifier.py
@@ -7,12 +7,13 @@ import nltk
 import string
 import numpy as np
 import matplotlib.pyplot as plt
+import FeatureGenerator
+from FeatureGenerator import *
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import precision_recall_fscore_support, accuracy_score, confusion_matrix, make_scorer, f1_score, confusion_matrix
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from collections import Counter
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from gensim import corpora
 from gensim.models import LdaModel
 from sklearn.preprocessing import RobustScaler
@@ -23,186 +24,7 @@ TEST_FILE = "civility_data/test.tsv"
 DEMOGRAPHIC_FILE = "civility_data/mini_demographic_dev.tsv"
 DEV_FILE = "civility_data/dev.tsv"
 
-#credit for this list goes to better_profanity package
-profane_words = pd.read_csv("Profanity_Wordlist.txt", header =None, names = ['word'])
-profane_words = profane_words['word'].tolist()
-
-#global var for threshold
-set_threshold = 0.5
-#build model using train to classify offensive vs. not tweets
-#report results on dev 
-#report FPR over mini_demographic
-#predict on test
-
-def preprocess(df: pd.DataFrame) -> pd.DataFrame:
-    """Preprocess data by tokenizing, lowercasing, and removing stopwords. 
-
-    Args:
-        df (pd.Dataframe): Pandas dataframe of containing 'text' column to process.
-
-    Returns:
-        pd.Dataframe: Original df with new column 'clean_text'.
-    """
-    nltk.download('stopwords')
-    stop_words = set(stopwords.words('english'))
-    stop_words.add("user")
-
-    text = df['text'].str.lower()
-    
-    def remove_punctuation(tweet):
-        return ''.join(char for char in tweet if char not in string.punctuation)
-
-    text = text.apply(remove_punctuation)
-
-    text = text.apply(word_tokenize)
-    
-    def filter_stopwords(sentence):
-        return [w for w in sentence if not w.lower() in stop_words]
-
-    text = text.apply(filter_stopwords)
-    
-    df['clean_text'] = text
-    return df
-
-def count_punctuation(df:pd.DataFrame)-> pd.DataFrame:
-    """Counts the number of punctuation marks in each tweet and removes them. 
-
-    Args:
-        df (pd.Dataframe): Pandas df containing a column called "text".
-        
-    Returns:
-        pd.Dataframe: Pandas dataframe with punctuation counts
-    """
-    punctuation = string.punctuation
-
-    def count_tweet(tweet): 
-        punct_count =  Counter(char for char in tweet if char in punctuation)
-        return {p: punct_count.get(p, 0) for p in punctuation}
-
-    text = df['text']
-    
-    punctuation_counts = text.apply(count_tweet)
-    
-    X_counts = pd.DataFrame(punctuation_counts.tolist(), columns=list(punctuation))
-
-    X_feature = pd.merge(df, X_counts, left_index=True, right_index=True)
-    
-    return X_feature
-
-def capital_ratio(df: pd.DataFrame)->pd.DataFrame:
-    """Generates new column with the ratio of capital to non-capital letters.
-
-    Args:
-        df (pd.DataFrame): Pandas df containing the columm 'text'
-
-    Returns:
-        pd.DataFrame: Same df with a new column 'capitals'
-    """
-    #remove punctuation to prevent skew
-    def remove_punctuation(tweet):
-        return ''.join(char for char in tweet if char not in string.punctuation)
-
-    text = df['text'].apply(remove_punctuation)
-    
-    #remove @USER to prevent skew
-    def remove_user(tweet: str):
-        return tweet.replace("USER", "")
-    
-    text = text.apply(remove_user)
-
-    #helper function to calculate capital ratio
-    def cap_count(tweet: str) -> float:
-        total_chars = len(tweet)
-        upper_chars = sum(1 for char in tweet if char.isupper())
-        return upper_chars / total_chars if total_chars > 0 else 0
-
-    df['capitals'] = text.apply(cap_count)
-    return df
-
-def sentiment_analysis(df: pd.DataFrame) -> pd.DataFrame:
-    """Generates sentiment scores using nltk sid module. 
-
-    Args:
-        df (pd.Dataframe): Df with column 'text' to analyze.
-
-    Returns:
-        pd.DataFrame: Df with additional columns negative, neutral, positive, and compound sentiment scores. 
-    """
-    
-    nltk.download('vader_lexicon')
-    sid = SentimentIntensityAnalyzer()
-
-    def get_sentiment_scores(text):
-        return sid.polarity_scores(text)
-    
-    df['sentiment_scores'] = df['text'].apply(get_sentiment_scores)
-
-    # Create separate columns for each sentiment score
-    df['negative'] = df['sentiment_scores'].apply(lambda x: x['neg'])
-    df['neutral'] = df['sentiment_scores'].apply(lambda x: x['neu'])
-    df['positive'] = df['sentiment_scores'].apply(lambda x: x['pos'])
-    df['compound'] = df['sentiment_scores'].apply(lambda x: x['compound'])
-
-    # Drop original sentiment score column
-    df = df.drop('sentiment_scores', axis=1)
-    
-    return df
-
-def generate_topics(df: pd.DataFrame)-> pd.DataFrame:
-    """Generates topics use lda module from genism. 
-
-    Args:
-        df (pd.DataFrame): Df containing column 'text' to be analyzed.
-
-    Returns:
-        pd.DataFrame: Df of features updated with topic column. 
-    """
-    text = df['clean_text']
-    dictionary = corpora.Dictionary(text)
-    corpus = [dictionary.doc2bow(text) for text in text]
-    
-    lda_model = LdaModel(corpus=corpus, id2word=dictionary, num_topics=10)
-    
-    def get_topics(tweet):
-        bow = dictionary.doc2bow(tweet)
-        return max(lda_model[bow], key=lambda x: x[1])[0] if lda_model[bow] else -1
-    
-    df['topic'] = text.apply(get_topics)
-    df['topic'] = df['topic'].astype('category')
-    
-    return df
-
-def str_length(df: pd.DataFrame)-> pd.DataFrame:
-    """Generates length column that describes length of original "text" column. 
-
-    Args:
-        df (pd.DataFrame): Pandas df containing column "text".
-
-    Returns:
-        pd.DataFrame: Original df with new column "length".
-    """
-    
-    df['length'] = df['text'].apply(len)
-    
-    return df
- 
-def count_profanity(df: pd.DataFrame)-> pd.DataFrame:
-    """_summary_
-
-    Args:
-        df (pd.DataFrame): _description_
-
-    Returns:
-        pd.DataFrame: _description_
-    """
-    def count_profane(tweet: list) -> int:
-        return sum(1 for word in tweet if word in profane_words)
-    
-    df['profanity'] = df['text'].apply(count_profane)
-    
-    return df
-
-def generate_analysis(df: pd.DataFrame) -> pd.DataFrame:
+def generate_features(df: pd.DataFrame) -> pd.DataFrame:
     """Generate features from tweets
 
     Args:
@@ -211,21 +33,19 @@ def generate_analysis(df: pd.DataFrame) -> pd.DataFrame:
     Returns:
         pd.DataFrame: Dataframe with engineered features
     """
-    
-    #pre process to remove stopwords, lowercase, and tokenize
-    X_feature = preprocess(df)
+        
+    feature_generator = FeatureGenerator(df)
     
     #create features
-    X_feature = count_punctuation(X_feature)        
-    X_feature = str_length(X_feature)    
-    X_feature = capital_ratio(X_feature)
-    X_feature = count_profanity(X_feature)
-    X_feature = sentiment_analysis(X_feature)
-    X_feature = generate_topics(X_feature)
-    
-    return X_feature
+    feature_generator.add_punctuation_count()
+    feature_generator.add_str_length()
+    feature_generator.add_capital_ratio()
+    feature_generator.add_count_profanity()
+    feature_generator.add_sentiment_analysis()
+    feature_generator.scale_features()
+    return feature_generator.get_features()
 
-def generate_features(df: pd.DataFrame, file: str) -> pd.DataFrame:
+def generate_features_df(df: pd.DataFrame, file: str) -> pd.DataFrame:
     """Generates features based on text column in df using multiple frameworks. Saves to file or pulls from file if it exsists.
 
     Args:
@@ -243,26 +63,12 @@ def generate_features(df: pd.DataFrame, file: str) -> pd.DataFrame:
         with open(filename, 'rb') as f:
             features = pickle.load(f)
     else:
-        features = generate_analysis(df)
+        features = generate_features(df)
         features = features.drop(['text', 'clean_text', 'label', 'category', 'demographic', 'perspective_score'], axis=1, errors='ignore')
         with open(filename, 'wb') as f:
             pickle.dump(features, f)
             
     return features
-
-def scale_features(df: pd.DataFrame) -> pd.DataFrame:
-    """Scales the features in a df using robust scaler from scikit-learn.
-
-    Args:
-        df (pd.DataFrame): Pandas df containing features.
-
-    Returns:
-        pd.DataFrame: Original df with features scaled. 
-    """
-    #scale features to improve model performance
-    scaler = RobustScaler()
-    X_scaled = pd.DataFrame(scaler.fit_transform(df))
-    return X_scaled
 
 def false_positive_rate(y_true, y_pred):
     tn, fp, fn, tp = confusion_matrix(y_true, y_pred,labels = ['NOT', 'OFF']).ravel()
@@ -454,8 +260,7 @@ if __name__ == "__main__":
     print("Data imported successfully!")
     
     #generate features and scales (scaling features improves model performance)
-    X_train = generate_features(df_train, "features")
-    X_train = scale_features(X_train)
+    X_train = generate_features(df_train)
     print("Features generated successfully!")
     
     #train model
@@ -463,10 +268,8 @@ if __name__ == "__main__":
     print("Model Trained!")
     
     #generate features
-    X_dev = generate_features(df_dev, "dev")
-    X_dev = scale_features(X_dev)
-    X_demographics = generate_features(df_demographic, "demographics")
-    X_demographics = scale_features(X_demographics)
+    X_dev = generate_features(df_dev)
+    X_demographics = generate_features(df_demographic)
     print("Dev features generated")
     
     #adjust threshold 
